d56ae067df0d588e6da5b0f5a7ecca2d03f12b4c
XiaoYaoNet <XiaoYaoNet@test.com>
2018-08-08 15:34:08 +0800

    C++11 1

---
+++ C++11(1)/ConsoleApplication1/ConsoleApplication1/Main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <array>
+
+using namespace std;
+int main()
+{
+	/*
+	long long a = 10000;
+	char16_t ch1 = 'q';
+	char32_t ch2 = '\U0000222B';
+	cout << a<<","<<sizeof(long long) << endl;
+	cout << ch1 <<","<< sizeof(char16_t) << endl;
+	cout << ch2 << "," << sizeof(char32_t) << endl;
+
+	auto au1 = 5.12345678;
+	auto au2 = 5.1;
+	auto au3 = 5;
+	auto au4 = 'q';
+	auto au5 = &au4;
+	decltype (au1) decl1 = 5;
+	cout << au1<<"," << sizeof(au1) << endl;
+	cout << au2 << "," << sizeof(au2) << endl;
+	cout << au3 << "," << sizeof(au3) << endl;
+	cout << au4 << "," << sizeof(au4) << endl;
+	cout << *au5 << "," << sizeof(au5) << endl;
+	cout << decl1 << "," << sizeof(decl1) << endl;
+
+	double arr1[5]{ 1.1, 2.2, 3.3, 4.4, 5.5 };
+	float arr2[5]{};
+	for (int i = 0; i < 5; i++)
+	cout << arr1[i] << " ";
+	cout << endl;
+	for (int i = 0; i < 5; i++)
+	cout << arr2[i] << " ";
+	cout << endl;
+
+	char ch1[]{"This is the first example."};
+	string str1{ "This is the second example" };
+	cout << ch1 << endl;
+	cout << str1 << endl;
+	
+	array <float, 4> array1={1.1,1.2,1.3,1.4};
+	for (int i = 0; i < 4; i++)
+		cout << array1[i] << " ";
+	cout << endl;
+	
+	double arr3[]{1.1, 2.2, 3.3, 4.4, 5.5};
+	for (double x : arr3)
+		cout << x << " ";
+	cout << endl;
+	
+	double &&rref = sqrt(36);
+	double j = 15;
+	double &&jref = 2 * j + 18.5;
+	cout << rref << ',' << jref << endl;
+	*/
+	char *ptr = nullptr;
+	char *psr = 0;
+	return 0;
+}
